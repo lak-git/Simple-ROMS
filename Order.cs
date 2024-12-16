ce8aa20929272340449ca1bfc663bb610d9bfb77
Lakindu Hesara Perera <159991932+lak-git@users.noreply.github.com>
2024-12-16 16:18:05 +0530

    feat: Initial Order class

---
+++ Order.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Simple_ROMS
+{
+    internal class Order
+    {
+        public List<FoodItem> OrderedFood { get; set; }
+
+        public Order()
+        {
+        }
+    }
+}
