4baa1c88e09d4dde9ff7338f7c48935a6b623377
Yumna-H <yumnahassan06@gmail.com>
2024-12-16 16:39:42 +0530

    Update Meal.cs

--- Meal.cs
+++ Meal.cs
@@ -6,7 +6,13 @@ using System.Threading.Tasks;
 
 namespace Simple_ROMS
 {
-    internal class Meal
+    internal class Meal : FoodItem
     {
+        public Meal(string name, float baseprice) : base(name, baseprice) { }
+
+        public override float CalculatePrice()
+        {
+            return Price * 1.18f;
+        }
     }
 }
